2b7fade64206d75547f524853baef21cdaecc058
nishthayadav456 <nishthayadav1122@gmail.com>
2023-09-09 10:58:30 +0530

    mss

--- src/Compo/FuncCom.js
+++ src/Compo/FuncCom.js
@@ -5,7 +5,7 @@ function FuncCom(){
             <h2 className="fun">This is created Using functional Component</h2>
             <p className='ss'>This is done using external css</p>
             <p style={{color:"red"}}>This is done using inline css</p>
-            <p></p>
+          
         </div>
     )
 }
